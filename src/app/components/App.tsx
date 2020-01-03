429f01ebddfbdad2e3a85575dc6c846f512d4c8a
elzup <guild0105@gmail.com>
2020-01-03 19:17:00 +0900

    feat: firebase configs

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,6 @@
 import Header from './Header'
 
-const App = ({ children }: { children?: any }) => (
+const App = ({ children }: { children?: unknown }) => (
   <main>
     <Header />
     {children}
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,7 +1,45 @@
-import App from '../components/App'
+import { useState, useEffect } from 'react'
+import { Button, Input } from '@material-ui/core'
+import firebase from 'firebase'
+import { initializeFirebase } from '../service/firebase'
 
-export default () => (
-  <App>
-    <p>Index Page</p>
-  </App>
-)
+import App from '../components/App'
+// Required for side-effects
+import 'firebase/firestore'
+
+initializeFirebase()
+const fdb = firebase.firestore()
+
+function Home() {
+  const [text, setText] = useState<string>('')
+
+  useEffect(() => {
+    console.log()
+  }, [])
+
+  return (
+    <div>
+      <Input
+        value={text}
+        onChange={e => {
+          setText(e.target.value)
+        }}
+      ></Input>
+      <Button
+        onClick={() => {
+          console.log('log')
+        }}
+      >
+        記録
+      </Button>
+    </div>
+  )
+}
+
+export default () => {
+  return (
+    <App>
+      <Home />
+    </App>
+  )
+}
