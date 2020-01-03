8f409383e5b1d4d7b83a6e2423e1c7820be810c5
elzup <guild0105@gmail.com>
2020-01-03 22:55:55 +0900

    firebase connect

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,3 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const webpack = require('webpack')
+
+require('dotenv').config()
+
 module.exports = {
   distDir: '../../dist/functions/next',
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+      return acc
+    }, {})
+
+    config.plugins.push(new webpack.DefinePlugin(env))
+
+    return config
+  },
 }
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from 'react'
 import { Button, Input } from '@material-ui/core'
 import firebase from 'firebase'
+import { useCollection } from 'react-firebase-hooks/firestore'
 import { initializeFirebase } from '../service/firebase'
-
 import App from '../components/App'
-// Required for side-effects
 import 'firebase/firestore'
+
+const userId = 'OegHtdhSLTtslN2L3YJn'
 
 initializeFirebase()
 const fdb = firebase.firestore()
 
 function Home() {
   const [text, setText] = useState<string>('')
+  const [actions, loading, error] = useCollection(
+    fdb
+      .collection('user')
+      .doc(userId)
+      .collection('actions')
+  )
 
-  useEffect(() => {
-    console.log()
-  }, [])
+  console.log(actions?.docs)
 
   return (
     <div>
